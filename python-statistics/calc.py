eaa57c7b01e8bebc74cc25dbb6a725822f9f7a00
TulioNPL <tulionp.lopes@gmail.com>
2020-03-22 13:45:04 -0300

    Codigo reorganizado + criada classe para comunicacao externa

--- python-statistics/calc.py
+++ python-statistics/calc.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-tipPDF = False #bool que decide o tipo a ser salvo
+class calc:
+	def grafCasosAtivos(tipPDF):
+		"""Cria um grafico com os paises com mais casos ativos"""
 
-df = pd.read_csv('dadosPaises.csv') #dataFrame que recebe a tabela de dados
-df = df.sort_values(by=['Casos Ativos'], ascending=False) #ordena pela coluna 'casos ativos' de forma descendente
-df2 = df[:5] # Separa em um novo dataframe as 5 primeiras linhas
+		df = pd.read_csv('dadosPaises.csv') #dataFrame que recebe a tabela de dados
+		df = df.sort_values(by=['Casos Ativos'], ascending=False) #ordena pela coluna 'casos ativos' de forma descendente
+		df2 = df[:5] # Separa em um novo dataframe as 5 primeiras linhas
 
-plt.bar(df2['Dados do País'],df2['Casos Ativos'],color='r') #cria o grafico de barras (nomes,valores)
-plt.ylabel('Quantidade de casos ativos') #label do eixo y
+		plt.bar(df2['Dados do País'],df2['Casos Ativos'],color='r') #cria o grafico de barras (nomes,valores)
+		plt.ylabel('Quantidade de casos ativos') #label do eixo y
 
-#Se true, o tipo selecionado sera pdf, senao png
-if tipPDF:
-	nomFig = 'grafico.pdf'
-else :
-	nomFig = 'grafico.png'
+		#Se true, o tipo selecionado sera pdf, senao png
+		if tipPDF:
+			nomFig = 'grafico.pdf'
+		else :
+			nomFig = 'grafico.png'	
+		plt.savefig(nomFig) #salva a imagem (poder ser salvo como pdf mudando o nome do arquivo
 
-plt.savefig(nomFig) #salva a imagem (poder ser salvo como pdf mudando o nome do arquivo
+
+#TestDriver
+calc.grafCasosAtivos(True)
+print(calc.grafCasosAtivos.__doc__)
